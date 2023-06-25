313d5c237b1e7d2267107d3139bf4f920da14a79
Vikkyava <adewoyevictoradedeji@gmail.com>
2023-06-25 14:06:09 +0100

    Modified code

--- src/components/SelectTip.js
+++ src/components/SelectTip.js
@@ -1,19 +1,27 @@
 import React from "react";
 
-export default function SelctTip() {
-    return(
+const SelectTip = ({handleTipChange, tip}) => {
+    const tipValues = [5, 10, 15, 25, 50]
+
+
+    return (
         <div className="main-bill-ctn">
             <p className="tip-header">Select Tip %</p>
-            <div className="tip-ctn">
-                <ul className="tip-percent-list">
-                    <li className="list-item">5%</li>
-                    <li className="list-item">10%</li>
-                    <li className="list-item">15%</li>
-                    <li className="list-item">25%</li>
-                    <li className="list-item">50%</li>
-                    <li className="list-item">Custom</li>
-                </ul>
-            </div>
+                <div className="tip-percent-list">
+                    {tipValues.map((value) => {
+                        return <button className="list-item" key={value} onClick={() => handleTipChange(value)}>{value}%</button>
+                    })}
+                    <input
+                        type="text"
+                        placeholder="Custom"
+                        className="list-item"
+                        id="item"
+                        value={tip}
+                        onChange={(e) => handleTipChange(e.target.value)}
+                    />
+                </div>
         </div>
     );
-}
+};
+
+export default SelectTip;
